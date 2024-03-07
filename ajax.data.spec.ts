2251ef3968a791266a0b43a10540686d9211c87e
Carlos Nieto <99897631+FlyerBird@users.noreply.github.com>
2024-03-07 08:38:02 +0100

    Add files via upload

---
+++ ajax.data.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Element appear after the AJAX, Wait for an Element to Show up', async ({ page }) => {
+    await page.goto('http://www.uitestingplayground.com/ajax');
+
+    const buttonAjax = 'button.btn-primary';
+    const textElement = 'p.bg-success';
+
+    await page.click(buttonAjax);
+
+    await page.waitForSelector(textElement);
+
+    await page.click(textElement);
+
+    expect (textElement).toBeTruthy();
+})
